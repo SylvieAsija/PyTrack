72f20a27d04def4978f9edeebdd5585740ce769b
group117 <group117@elec377.queensu.ca>
2024-11-13 00:51:05 -0500

    repo initialization complete and working

--- git_repository.py
+++ git_repository.py
@@ -10,23 +10,23 @@ class GitRepository (object):
     
     def __init__(self, path, force=False):
         self.worktree = path
-        self.gitdir = os.path.join(path, ".git")
+        self.gitdir = os.path.join(path, '.git')
         
         if not (force or os.path.isdir(self.gitdir)):
-            raise Exception("Not a Valid Git Repository ", path)
+            raise Exception('Not a Valid Git Repository ', path)
         
         self.conf = configparser.ConfigParser()
-        cf = repo_file(self, "config")
+        cf = repo_file(self, 'config')
         
         if cf and os.path.exists(cf):
             self.conf.read([cf])
         elif not force:
-            raise Exception("Configuration File Missing")
+            raise Exception('Configuration File Missing')
         
         if not force:
-            vers = int(self.conf.get("core", "repositoryformatversion"))
+            vers = int(self.conf.get('core', 'repositoryformatversion'))
             if vers != 0:
-                raise Exception("Unsupported Repository Format Version ", vers)
+                raise Exception('Unsupported Repository Format Version ', vers)
 
 def repo_path(repo, *path):
     return os.path.join(repo.gitdir, *path)
@@ -42,11 +42,64 @@ def repo_dir(repo, *path, mkdir=False):
         if os.path.isdir(path):
             return path
         else:
-            raise Exception("Not a Valid Directory ", path)
+            raise Exception('Not a Valid Directory ', path)
     
     if mkdir:
         os.makedirs(path)
         return path
     else:
         return None
+    
+def repo_create(path):
+    repo = GitRepository(path, True)
+    
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception(path, ' is Not a Directory')
+        if os.path.exists(repo.gitdir):
+            raise Exception(path, ' is Not Empty')
+    else:
+        os.makedirs(repo.worktree)
         
+    assert repo_dir(repo, 'branches', mkdir=True)
+    assert repo_dir(repo, 'objects', mkdir=True)
+    assert repo_dir(repo, 'refs', 'tags', mkdir=True)
+    assert repo_dir(repo, 'refs', 'heads', mkdir=True)
+    
+    with open(repo_file(repo, 'description'), 'w') as f:
+        f.write('Unnamed repository; edit this file \'description\' to name the repository')
+    
+    with open(repo_file(repo, 'HEAD'), 'w') as f:
+        f.write('ref: refs/heads/master\n')
+        
+    with open(repo_file(repo, 'config'), 'w') as f:
+        config = repo_default_config()
+        config.write(f)
+    
+    return repo
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+    
+    ret.add_section('core')
+    ret.set('core', 'repositoryformatversion', '0')
+    ret.set('core', 'filemode', 'false')
+    ret.set('core', 'bare', 'false')
+    
+    return ret
+
+def repo_find(path="", required=True):
+    path = os.path.realpath(path)
+    
+    if os.path.isdir(os.path.join(path, ".git")):
+        return GitRepository(path)
+
+    parent = os.path.realpath(os.path.join(path, ".."))
+    
+    if parent == path:
+        if required:
+            raise Exception("No git directory")
+        else:
+            return None
+    
+    return repo_find(parent, required)
--- libwyag.py
+++ libwyag.py
@@ -11,26 +11,33 @@ import re
 import sys
 import zlib
 
-argparser = argparse.ArgumentParser()
-argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
-argsubparsers.required = True
+import git_repository
 
+argparser = argparse.ArgumentParser()
+argsubparsers = argparser.add_subparsers(title='Commands', dest='command')
+argsubparsers.required = True
+argsp = argsubparsers.add_parser('init', help='Initialize a new, empty repository')
+argsp.add_argument('path', metavar='directory', nargs='?', default='.', help='Where to create the repository')
+    
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     match args.command:
-        case "add"          : cmd_add(args)
-        case "cat-file"     : cmd_cat_file(args)
-        case "check-ignore" : cmd_check_ignore(args)
-        case "checkout"     : cmd_checkout(args)
-        case "commit"       : cmd_commit(args)
-        case "hash-object"  : cmd_hash_object(args)
-        case "init"         : cmd_init(args)
-        case "log"          : cmd_log(args)
-        case "ls-files"     : cmd_ls_files(args)
-        case "ls-tree"      : cmd_ls_tree(args)
-        case "rev-parse"    : cmd_rev_parse(args)
-        case "rm"           : cmd_rm(args)
-        case "show-ref"     : cmd_show_ref(args)
-        case "status"       : cmd_status(args)
-        case "tag"          : cmd_tag(args)
-        case _              : print("Invalid Command")
+        # case 'add'          : cmd_add(args)
+        # case 'cat-file'     : cmd_cat_file(args)
+        # case 'check-ignore' : cmd_check_ignore(args)
+        # case 'checkout'     : cmd_checkout(args)
+        # case 'commit'       : cmd_commit(args)
+        # case 'hash-object'  : cmd_hash_object(args)
+        case 'init'         : cmd_init(args)
+        # case 'log'          : cmd_log(args)
+        # case 'ls-files'     : cmd_ls_files(args)
+        # case 'ls-tree'      : cmd_ls_tree(args)
+        # case 'rev-parse'    : cmd_rev_parse(args)
+        # case 'rm'           : cmd_rm(args)
+        # case 'show-ref'     : cmd_show_ref(args)
+        # case 'status'       : cmd_status(args)
+        # case 'tag'          : cmd_tag(args)
+        case _              : print('Invalid Command')
+        
+def cmd_init(args):
+    git_repository.repo_create(args.path)
